5c89e10706b826d15894774eaf3d7674550ec858
周立翊 <lzhou1110@gmail.com>
2019-05-07 23:16:50 +0100

    added wait script

--- run_docker_stack.sh
+++
@@ -1,6 +0,0 @@
-docker container prune
-docker stack rm bitcoinstack
-./build_bitcoind_base.sh
-./build_bitcoind_original.sh
-./build_attacker.sh
-docker stack deploy -c docker-compose.yml bitcoinstack
